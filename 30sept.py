71e9db73dcc67af4a5829eacbeb1857c6ec9d452
dishant1123 <dishantshah1417@gmail.com>
2025-09-30 15:28:32 +0530

    ...

--- 30sept.py
+++ 30sept.py
@@ -107,7 +107,7 @@ p=person(10,20,30,name="saumya",age=21,clg="old lj")
 p.show() # ==> args  
 p.display() # ==> kwargs
 """
-
+"""
 class employees:
     def __init__(self,name,age,clg):
         self.name =name
@@ -124,3 +124,68 @@ clg =input("enter clg : ")
 
 e=employees(name,age,clg)
 e.show()
+"""
+# hw  : 
+"""
+employees management  system  using class and object, constructor ,*args,**kargs :
+ 
+1. add 
+2. delete 
+3. update 
+4. search
+5. display
+6.exit   
+"""
+
+# 4 pillar  : 
+"""
+1. inheritance : to inherit properties and methods from parent class
+2. polymorphism : to use same method name for different classes
+    a. method  overloading
+    b. method  overriding
+3. encapsulation : to hide properties and methods from outside
+    a. set method 
+    b. get method
+4. abstraction : to hide implementation details
+    a. abstract  base class 
+    b. abstract  method
+"""
+
+# inheritance : to inherit properties and methods from parent class
+
+#1. single level inheritance :
+
+"""class parent :  # base class  
+    def show(self):
+        print("parent class")
+
+class child(parent):  # derived class
+    pass 
+
+c=child()
+c.show()
+"""
+
+# using  constructor  : 
+
+class vehicle :
+    def __init__(self):
+        self.name ="four wheeler"  # non para 
+        self.capacity_of_seating=5 
+    def show(self):
+        print("vehicle name is  :",self.name)
+        print("capacity of seating is  :",self.capacity_of_seating)
+
+class car(vehicle):
+    def __init__(self,model,color):
+        super().__init__()  # base class constructor  called 
+        # vehicle.__init__(self)
+        self.model =model   # para 
+        self.color =color
+    def display(self):
+        self.show()
+        print("model is  :",self.model)
+        print("color is  :",self.color)
+
+c=car("M-4","black")
+c.display()
